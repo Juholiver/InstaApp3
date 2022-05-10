bf35951bc0eff5b6ae27919311b9e4878a608c98
juholiver <juholiver3@gmail.com>
2022-05-10 19:43:21 -0300

    UsandoCallBack

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -21,7 +21,10 @@ import { Component, OnInit } from '@angular/core';
       })),
       transition('void => criado', [
         style({opacity: 0, transform: 'translate(50px, 0)'}),
-        animate('500ms 0s ease-in-out')
+
+// keyframes          0.15                   0.86    0.88 0.90               1.5
+        //0 void -----X-----------------------X-----X---X-------------------X criado 1.5s//
+        animate('1500ms 0s ease-in-out')
       ])
     ])
   ]
@@ -41,5 +44,13 @@ export class AcessoComponent implements OnInit {
   public exibirPainel(event: string): void {
     this.cadastro = event ==='cadastro' ? true : false
   }
+
+  public inicioDaAnimacao(): void {
+    console.log('inicio da animação')
+  }
+  public fimDaAnimacao(): void {
+    console.log('fim da animação')
+  }
+
 
 }
