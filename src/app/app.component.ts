57d05179c9cb72b38fe79b31c5ebcd036fdbcf8d
juholiver <juholiver3@gmail.com>
2022-05-11 11:47:29 -0300

    ConectandooFirebaseSDKComProjetoBackEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app3.1';
+
+  ngOnInit(): void {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyAAtObvkWb4Tf6kabpBu5tihShClYoKImg",
+      authDomain: "jta-instagram-f.firebaseapp.com",
+      projectId: "jta-instagram-f",
+      storageBucket: "jta-instagram-f.appspot.com",
+      messagingSenderId: "1083884512778",
+      appId: "1:1083884512778:web:0803d873d83afe7e6ee1f3",
+      measurementId: "G-GJTNC6P31P"
+    };
+    
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
