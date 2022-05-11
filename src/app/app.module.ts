f0de16fa48a2808f983f27d9f85c72a5f4daeddb
juholiver <juholiver3@gmail.com>
2022-05-11 14:41:58 -0300

    CriandoHomeEPost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
 import { LoginComponent } from './acesso/login/login.component';
 import { CadastroComponent } from './acesso/cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     AcessoComponent,
     BannerComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    HomeComponent,
+    PublicacoesComponent,
+
   ],
   imports: [
     BrowserModule,
