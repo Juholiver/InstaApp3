eb88527942a5de288a0b1f3ac90a9384ec0ff0a2
juholiver <juholiver3@gmail.com>
2022-05-11 15:02:36 -0300

    AdcRotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { ROUTES } from './app.routes';
 
 import { Autenticacao } from './autenticacao.service';
 
@@ -30,8 +33,10 @@ import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
+
   providers: [Autenticacao],
   bootstrap: [AppComponent]
 })
