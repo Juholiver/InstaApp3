c405e18d76228f702d7d0d27d5bde01a5f4bbf2e
juholiver <juholiver3@gmail.com>
2022-05-12 14:29:32 -0300

    NvPublicacaoViewChild

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Autenticacao } from '../autenticacao.service';
 @Component({
   selector: 'app-home',
@@ -6,6 +6,8 @@ import { Autenticacao } from '../autenticacao.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  @ViewChild('publicacoes') public publicacoes: any
 
   constructor(
     private autenticacao: Autenticacao
@@ -20,7 +22,7 @@ export class HomeComponent implements OnInit {
   }
 
   public atualizarTimeLine(): void {
-    console.log('chegamos ate aqui')
+    this.publicacoes.atualizarTimeLine()
   }
 
 }
