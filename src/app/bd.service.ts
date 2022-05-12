8dec9ca35f613900f38e0a526da03bcfce287269
juholiver <juholiver3@gmail.com>
2022-05-12 13:12:48 -0300

    GravaçãoStorate

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -9,32 +9,41 @@ export class Bd {
     constructor(private progresso: Progresso) { }
     public publicar(publicacao: any): void {
 
-        let nomeImagem = Date.now()
+       
 
-        firebase.storage().ref()
-            .child(`imagens/${nomeImagem}`)
-            .put(publicacao.imagem)
-            .on(firebase.storage.TaskEvent.STATE_CHANGED,
-                //acompanhamento
-                (snapshot: any) => {
-                    this.progresso.status = 'andamento'
-                    this.progresso.estado = snapshot
-                    //console.log(snapshot)
-                },
-                (error) => {
-                    this.progresso.status = 'erro'
-                    //console.log(error)
-                },
-                ()=> {
-                    //finalização do processo
-                    this.progresso.status = 'concluido'
-                    //console.log('upload completo')
-                }
-                
-            )
-/*
         firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
-            .push( { titulo:publicacao.titulo})*/
-        //console.log('chegamos ate o servico responsavel')
+        .push( { titulo:publicacao.titulo })
+        .then((resposta: any) => {
+
+            let nomeImagem = resposta.key
+
+            firebase.storage().ref()
+                .child(`imagens/${nomeImagem}`)
+                .put(publicacao.imagem)
+                .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                    //acompanhamento
+                    (snapshot: any) => {
+                        this.progresso.status = 'andamento'
+                        this.progresso.estado = snapshot
+                        //console.log(snapshot)
+                    },
+                    (error) => {
+                        this.progresso.status = 'erro'
+                        //console.log(error)
+                    },
+                    ()=> {
+                        //finalização do processo
+                        this.progresso.status = 'concluido'
+                        //console.log('upload completo')
+                    })
+          
+        })
+
+        
+                
+            
+
+       
+        
     }
 }
